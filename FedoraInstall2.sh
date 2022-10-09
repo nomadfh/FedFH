ea7e2391389e8befb6daaa6d52917db17bfe46c9
nomadfh <adenjsdad@gmail.com>
2022-10-08 20:55:27 -0400

    removed -y from touchegg systemctl cmds

--- FedoraInstall2.sh
+++ FedoraInstall2.sh
@@ -5,12 +5,12 @@ sudo cp ~/.config/monitors.xml ~gdm/.config/ &&
 xhost SI:localuser:gdm
 sudo -u gdm gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true &&
 # install enable and start touchegg 
-sudo dnf copr enable -y joseexposito/touchegg &&
+sudo dnf copr enable joseexposito/touchegg &&
 sudo dnf install touchegg &&
-sudo systemctl enable -y touchegg.service &&
+sudo systemctl enable touchegg.service &&
 sudo systemctl start touchegg
 # Install System 76 Task Scheduler
-sudo dnf copr enable -y mjakeman/system76-scheduler &&
+sudo dnf copr enable mjakeman/system76-scheduler &&
 sudo dnf install system76-scheduler
 # Install System 76 Pop Shell extension and exclude keyboard shortcut overrides
 sudo dnf install -y --setopt=exclude=gnome-shell-extension-pop-shell-shortcut-overrides gnome-shell-extension-pop-shell &&
