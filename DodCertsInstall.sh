fc10192324745528fa9b9c74e61d9599e738fd31
nomadfh <97919313+nomadfh@users.noreply.github.com>
2023-03-16 10:53:24 -0400

    Update DodCertsInstall.sh
    New Certificates were released, changed download URL and appropriately modified folder names

--- DodCertsInstall.sh
+++ DodCertsInstall.sh
@@ -1,9 +1,9 @@
 
 # Download dod pki certificates
-wget https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/certificates_pkcs7_DoD.zip &&
-unzip certificates_pkcs7_DoD.zip &&
+wget https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/unclass-certificates_pkcs7_DoD.zip &&
+unzip unclass-certificates_pkcs7_DoD &&
 # change into the newly created certificate directory
-cd Certificates_PKCS7_v5.9_DoD &&
+cd certificates_pkcs7_v5_11_dod &&
 # import all of the p7b files into the trust store
 for n in *.p7b; do certutil -d sql:$HOME/.pki/nssdb -A -t TC -n $n -i $n; done ;
 # display status of installed certs on machine
@@ -12,6 +12,5 @@ cd $HOME
 # add CAC module to opensc
 modutil -dbdir sql:.pki/nssdb/ -add “CAC_Module” -libfile /usr/lib64/pkcs11/p11-kit-client.so &&
 mkdir DODcerts &&
-mv Certificates_PKCS7* DODcerts &&
-mv certificates_* DODcerts &&
+mv unclass-* DODcerts &&
 echo "All done!"
