57cdfe51d4e00bd2af7f7be54549e01ee32450b2
nomadfh <adenjsdad@gmail.com>
2023-06-29 19:28:45 -0400

    Added protontricks to rpm apps

--- Fedin.sh
+++ Fedin.sh
@@ -16,7 +16,7 @@ sudo dnf install \
 sudo dnf install -y \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&
 # install rpm applications
-sudo dnf install -y powertop htop gnome-tweaks wine neofetch vim timeshift cmatrix nss-tools pcsc-lite perl-pcsc pcsc-tools ccid opensc openrgb steam-devices &&
+sudo dnf install -y powertop protontricks htop gnome-tweaks wine neofetch vim timeshift cmatrix nss-tools pcsc-lite perl-pcsc pcsc-tools ccid opensc openrgb steam-devices &&
 # install flatpak applications
 flatpak install -y org.libreoffice.LibreOffice com.raggesilver.BlackBox com.valvesoftware.Steam net.lutris.Lutris org.videolan.VLC flathub com.github.tchx84.Flatseal flathub org.standardnotes.standardnotes com.mattjakeman.ExtensionManager net.davidotek.pupgui2 com.spotify.Client org.gnome.FontManager org.qbittorrent.qBittorrent flathub org.onlyoffice.desktopeditors io.github.aandrew_me.ytdn &&
 # Remove redundant graphical RPM applications
